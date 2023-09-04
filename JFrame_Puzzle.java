987c553893c2e92282ff714c2ccf3cb98a778643
Prathamesh-1011 <144042848+Prathamesh-1011@users.noreply.github.com>
2023-09-04 20:57:02 +0530

    Update JFrame_Puzzle.java

--- JFrame_Puzzle.java
+++ JFrame_Puzzle.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.puzzlesgame;
 
-/**
- *
- * @author Pranay Rokade
- */
+package com.mycompany.puzzlesgame;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
